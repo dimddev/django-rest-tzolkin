8bf169450532e794114ad5d4113e2cc21895922a
Dim Mitar Hakini <targolini@gmail.com>
2014-08-07 20:17:23 +0300

    install fix, url fix

--- tzolkin/urls.py
+++ tzolkin/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from .views import Gregorian2Tzolkin
 from .views import Tzolkin2Gregorian
 
-tzolkin_urls = patterns('',
+urlpatterns = patterns('',
     url(r'^/v1/ancient/calendars/mayan/gregorian2tzolkin', Gregorian2Tzolkin.as_view(), name='api-gregorian2tzolkin'),
     url(r'^/v1/ancient/calendars/mayan/tzolkin2gregorian', Tzolkin2Gregorian.as_view(), name='api-tzolkin2gregorian')
 )
