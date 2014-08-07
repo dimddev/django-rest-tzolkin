6d87db71d0e343cbe2885d44a08d67f637b99264
Dim Mitar Hakini <targolini@gmail.com>
2014-08-07 20:46:27 +0300

    same fixes

--- test_tzolkin/urls.py
+++ test_tzolkin/urls.py
@@ -1,21 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import patterns, url, include
-from django.conf import settings
-#from django.views.generic import TemplateView
-
-############################################
-
-
-############################################
-
-# client
-from tzolkin.urls import tzolkin_urls
 
 urlpatterns = patterns('',
 
-    url(r'^API', include(tzolkin_urls)),
+    url(r'^API', include('tzolkin.urls')),
 
 )
-
-############################################
 
